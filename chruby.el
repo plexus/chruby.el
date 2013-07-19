a15fb0fdca212e6ded2018dc38bc27dd3bdab568
Arne Brasseur <arne@arnebrasseur.net>
2013-07-19 14:06:32 +0200

    Make the parameter to chruby optional
    If omitted, the return value will be the name of the current Ruby.

--- chruby.el
+++ chruby.el
@@ -11,7 +11,7 @@
 
 ;; This file is NOT part of GNU Emacs.
 
-;;; License:
+ ;;; License:
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -28,11 +28,11 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
-;;; Attribution :
+ ;;; Attribution :
 
 ;; The chruby-change-path function was taken from rvm.el by Yves Senn
 
-;;; Commentary:
+ ;;; Commentary:
 
 ;; The chruby function functions like the shell command, pass it the
 ;; name of a Ruby such as 'ruby-1.9.3' or 'jruby-1.7.2'.
@@ -45,52 +45,56 @@
 
 (defvar chruby-current-ruby-binary-path nil
   "reflects the path to the current 'ruby' executable.
-This path gets added to the PATH variable and the exec-path list.")
+ This path gets added to the PATH variable and the exec-path list.")
+
+(defvar chruby-current-ruby-name nil
+  "The full name of the active Ruby, i.e. the name of the .rubies/
+ subdirectory.")
 
 (defun chruby-collect-rubies (rubies-dir)
   "Return the full directories found in the given directory"
   (cddr                    ; cddr : drop the first 2, in this case "." and ".."
    (and rubies-dir         ; not nil
-	(and
-	 (file-directory-p rubies-dir)
-	 (directory-files rubies-dir t)))))
+        (and
+         (file-directory-p rubies-dir)
+         (directory-files rubies-dir t)))))
 
 (defun chruby-rubies ()
   "Find all Rubies in various places, returns a list of directories."
   (mapcan
    'chruby-collect-rubies
    (list "/opt/rubies/"
-	 (concat (file-name-as-directory (getenv "HOME")) ".rubies")
-	 (getenv "RUBIES_DIR")
-	 "~/opt/rubies")))
+         (concat (file-name-as-directory (getenv "HOME")) ".rubies")
+         (getenv "RUBIES_DIR")
+         "~/opt/rubies")))
 
 (defun chruby-find (name)
   "Find the given ruby in the list of rubies"
   (first
    (delq nil
-	 (mapcar
-	  '(lambda (ruby)
-	     (and (string-match name ruby) ruby))
-	  (chruby-rubies)))))
+         (mapcar
+          '(lambda (ruby)
+             (and (string-match name ruby) ruby))
+          (chruby-rubies)))))
 
-(defun chruby-change-path (current-binary-var new-binaries)
+(defun chruby-change-path (new-binaries)
   (let ((current-binaries-for-path
-         (mapconcat 'identity (eval current-binary-var) ":"))
+         (mapconcat 'identity chruby-current-ruby-binary-path ":"))
         (new-binaries-for-path (mapconcat 'identity new-binaries ":")))
-    (if (and (eval current-binary-var)
-             (not (string= (first (eval current-binary-var)) "/bin")))
+    (if (and chruby-current-ruby-binary-path
+             (not (string= (first chruby-current-ruby-binary-path) "/bin")))
         (progn
           (setenv "PATH" (replace-regexp-in-string
                           (regexp-quote current-binaries-for-path)
                           new-binaries-for-path
                           (getenv "PATH")))
-          (dolist (binary (eval current-binary-var))
+          (dolist (binary chruby-current-ruby-binary-path)
             (setq exec-path (remove binary exec-path))))
       (setenv "PATH" (concat new-binaries-for-path ":" (getenv "PATH"))))
     (dolist (binary new-binaries)
       (add-to-list 'exec-path binary))
     (setq eshell-path-env (getenv "PATH"))
-    (set current-binary-var new-binaries)))
+    (setq chruby-current-ruby-binary-path new-binaries)))
 
 (defun chruby-set-gemhome (gemhome gempath)
   (if (and gemhome gempath)
@@ -102,26 +106,42 @@ This path gets added to the PATH variable and the exec-path list.")
     (setenv "GEM_PATH" "")
     (setenv "BUNDLE_PATH" "")))
 
-(defun chruby (name)
+(defun chruby (&optional name)
+  "If name is given, activate the given Ruby. Else, return the currently
+ activated Ruby"
+  (if name
+      (chruby-activate name)
+    (chruby-current)))
+
+(defun chruby-activate (name)
   "Activate the given Ruby"
   (let ((ruby-root (chruby-find name)))
-    (progn
-      (chruby-change-path
-       'chruby-current-ruby-binary-path
-       (list (concat ruby-root "/bin"))))
-    (let ((engine_version_gempath (split-string
-				   (shell-command-to-string
-				    "ruby -rubygems -e 'print [(defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'), (RUBY_VERSION), (Gem.default_dir.inspect)].join(%[##])'") "##")))
+    (setq chruby-current-ruby-name (chruby-util-basename ruby-root))
+
+    (chruby-change-path (list (concat ruby-root "/bin")))
+
+    (let ((engine_version_gempath (chruby-query-version)))
       (let ((engine (first engine_version_gempath))
-	    (version (second engine_version_gempath))
-	    (gemroot (third engine_version_gempath)))
-	(let ((gemhome (concat (getenv "HOME") "/.gem/" engine "/" version)))
-	  (progn
-	    (chruby-set-gemhome gemhome
-				(concat gemhome ":" gemroot))
-	    (chruby-change-path
-	     'chruby-current-ruby-binary-path
-	     (list (concat gemhome "/bin") (concat ruby-root "/bin"))))
-	)))))
+            (version (second engine_version_gempath))
+            (gemroot (third engine_version_gempath)))
+        (let ((gemhome (concat (getenv "HOME") "/.gem/" engine "/" version)))
+          (chruby-set-gemhome gemhome
+                              (concat gemhome ":" gemroot))
+          (chruby-change-path
+           (list (concat gemhome "/bin") (concat ruby-root "/bin"))))))))
+
+(defun chruby-query-version ()
+  "Shell out to Ruby to find out the current engine (ruby, jruby, etc), the
+ruby version, and the gem path"
+  (split-string
+   (shell-command-to-string
+    "ruby -rubygems -e 'print [(defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'), (RUBY_VERSION), (Gem.default_dir.inspect)].join(%[##])'") "##"))
+
+(defun chruby-util-basename (path)
+  (file-name-nondirectory (directory-file-name path)))
+
+(defun chruby-current ()
+  "Return the currently activated Ruby name"
+  chruby-current-ruby-name)
 
 (provide 'chruby)
